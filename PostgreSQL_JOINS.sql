50f43d4b73d024d83442be34854e95a047cca62f
NILU RA <70004867+nrathgama@users.noreply.github.com>
2022-02-28 23:51:04 -0600

    Update PostgreSQL_JOINS.sql

--- PostgreSQL_JOINS.sql
+++ PostgreSQL_JOINS.sql
@@ -17,7 +17,7 @@ ORDER BY a.name;
  associated accounts. This time only for accounts where the sales rep has a
  first name starting with S and in the Midwest region. Your final table should
  include three columns: the region name, the sales rep name, and the account
- name. Sort the accounts alphabetically (A-Z) according to account name./*
+ name. Sort the accounts alphabetically (A-Z) according to account name.*/
 
 SELECT r.name region, s.name rep, a.name account
 FROM sales_reps s
